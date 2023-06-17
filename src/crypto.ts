c548833430a3ef3bd7b5d922befab5d8bea0a8d2
Baker <baker@shamlan.sa>
2023-06-17 12:02:27 +0300

    Update crypto.ts

--- src/crypto.ts
+++ src/crypto.ts
@@ -5,11 +5,15 @@ const genericKey = 'a3K8Bx%2r8Y7#xDh';
 export default {
   encrypt: (data, key = genericKey) => {
     const string = JSON.stringify(data);
-    const cipher = crypto.createCipheriv('aes-128-ecb', key, null);
-    return cipher.update(string, 'utf8', 'base64') + cipher.final('base64');
+    const cipher = crypto.createCipher('aes-128-ecb', key); // Use createCipher instead of createCipheriv
+    let encrypted = cipher.update(string, 'utf8', 'base64');
+    encrypted += cipher.final('base64');
+    return encrypted;
   },
   decrypt: (data, key = genericKey) => {
-    const decipher = crypto.createDecipheriv('aes-128-ecb', key, null);
-    return JSON.parse(decipher.update(data, 'base64', 'utf8') + decipher.final('utf8'));
+    const decipher = crypto.createDecipher('aes-128-ecb', key); // Use createDecipher instead of createDecipheriv
+    let decrypted = decipher.update(data, 'base64', 'utf8');
+    decrypted += decipher.final('utf8');
+    return JSON.parse(decrypted);
   },
 };
